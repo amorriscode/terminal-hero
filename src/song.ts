f33d43a90de80d5be7629bcf478351fd76f6c27f
Anthony Morris <anthony@amorrissound.com>
2020-05-10 13:57:55 -0700

    Parse MIDI into Song class

--- src/song.ts
+++ src/song.ts
@@ -1,6 +1,9 @@
 import Speaker from 'speaker'
 import ffmpeg from 'fluent-ffmpeg'
 import Volume from 'pcm-volume'
+import {Midi} from '@tonejs/midi'
+import {Note} from '@tonejs/midi/dist/Note'
+import fs from 'fs'
 
 function createSpeaker() {
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
@@ -46,13 +49,37 @@ class Song {
 
   song: any;
 
+  notes: Note[];
+
+  songPath: string;
+
   constructor(songPath: string) {
-    this.guitar = new TrackStream(`${songPath}/guitar.ogg`)
+    this.songPath = songPath
+    this.notes = []
+  }
+
+  async init() {
+    this.guitar = new TrackStream(`${this.songPath}/guitar.ogg`)
     // Set guitar volume to 0 until player hits a button
     this.guitar.setVolume(0)
 
-    this.rhythm = new TrackStream(`${songPath}/rhythm.ogg`)
-    this.song = new TrackStream(`${songPath}/song.ogg`)
+    this.rhythm = new TrackStream(`${this.songPath}/rhythm.ogg`)
+    this.song = new TrackStream(`${this.songPath}/song.ogg`)
+
+    this.notes = await this.parseMidi()
+  }
+
+  async parseMidi() {
+    const midiData = fs.readFileSync(`${this.songPath}/notes.mid`)
+    const midi = new Midi(midiData)
+
+    midi.tracks.forEach(track => {
+      if (track.name === 'PART GUITAR') {
+        this.notes = track.notes
+      }
+    })
+
+    return []
   }
 }
 
